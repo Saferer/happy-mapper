9a117fb66350eca0a9d4007d94a5a9f14b9ad517
Saferer <james.nguy@shaw.ca>
2020-02-29 23:53:06 -0700

    Refactor

---
+++ HMSpace/Assets/Graph/Scripts/EMGReader.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO.Ports;
+
+
+namespace I4HUSB
+{
+    public class EMGReader
+    {
+        private int[] packetBytes;
+        private double[] pastValues;
+        private double deletedValue = 0;
+        private int pastIndex = 0;
+        private double basepoint = 300;
+        private int index = 2;
+        private double goal = 0;
+        private double runningAverage = 0;
+        SerialPort serialPort;
+        private bool keepRunning = true;
+        private double AVERAGE_PERIOD = 0.2;  // seconds
+        private double RATE = 252; // hz
+        private double CALIBRATION_TIME = 5; //seconds
+
+        //Constructor
+        public EMGReader()
+        {
+
+            //initializeProgram();
+            serialPort = new SerialPort("/dev/cu.usbmodem14201", 57600, Parity.None);
+            if (serialPort.IsOpen){
+              serialPort.Close();
+            }
+            serialPort.Open();
+            packetBytes = new int[17];
+
+            int size = Math.Round(RATE * AVERAGE_PERIOD);
+            pastValues = new double[size];
+        }
+
+        //Run this code on a serperate thread. This already loops so do not need to run this in loop
+        public void run()
+        {
+            while(keepRunning)
+            {
+                while (true)
+                {
+                    packetBytes[0] = serialPort.ReadByte();
+                    if (packetBytes[0] == 0xa5)
+                    {
+                        //Console.WriteLine("Found a5");
+                        packetBytes[1] = serialPort.ReadByte();
+                        if (packetBytes[1] == 0x5a)
+                        {
+                            //Console.WriteLine("Found 5a");
+                            break;
+                        }
+
+                    }
+                }
+                while (true)
+                {
+
+                    if (index > 16)
+                    {
+                        double[] channels = new double[6];
+                        double average = 0;
+                        for (int i = 0; i < channels.Length; i+=2)
+                        {
+                            channels[i] = transform((int)(packetBytes[i+4] << 8 | packetBytes[i+5]));
+                            average += channels[i];
+                        }
+
+                        /*
+                        Console.WriteLine(packetBytes[4] << 8 | packetBytes[5]);
+                        Console.WriteLine(packetBytes[6] << 8 | packetBytes[7]);
+                        Console.WriteLine(packetBytes[8] << 8 | packetBytes[9]);
+                        */
+                        average /= channels.Length;
+                        //max = average > max ? average : max;
+                        deletedValue = pastValues[pastIndex];
+
+                        pastValues[pastIndex++] = average;
+                        calculateRunningAverage(average);
+
+                        if (pastIndex >= pastValues.Length)
+                        {
+                            pastIndex = 0;
+                        }
+                        Console.WriteLine(getPercentage()); //comment this out later
+                        index = 2;
+                        break;
+                    }
+                    packetBytes[index++] = serialPort.ReadByte();
+
+                }
+            }
+            //Debug.Log(getPercentage());
+        }
+
+        //Transform values with zero reference
+        private double transform(double channel)
+        {
+            channel -= basepoint;
+            channel = Math.Abs(channel);
+            return channel;
+        }
+
+        //Calculates window
+        private void calculateRunningAverage(double newValue)
+        {
+            runningAverage += (newValue / pastValues.Length);
+            runningAverage -= (deletedValue / pastValues.Length);
+        }
+
+        //Get measured ratio of current window compared to max with a range of (0,2)
+        public double getPercentage()
+        {
+            double result = (double)runningAverage / goal;
+            if (result > 2) { result = 2; }
+            return result;
+        }
+
+        //Starts Calibration for zero point (When measuring base do not move the muscle)
+        public void calibrateBase()
+        {
+            serialPort.DiscardInBuffer();
+            double basepointAverage = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                while (true)
+                {
+                    packetBytes[0] = serialPort.ReadByte();
+                    if (packetBytes[0] == 0xa5)
+                    {
+                        //Console.WriteLine("Found a5");
+                        packetBytes[1] = serialPort.ReadByte();
+                        if (packetBytes[1] == 0x5a)
+                        {
+                            //Console.WriteLine("Found 5a");
+                            break;
+                        }
+
+                    }
+                }
+                while (true)
+                {
+
+                    if (index > 16)
+                    {
+                        double channel1 = (int)(packetBytes[4] << 8 | packetBytes[5]);
+                        double channel2 = (int)(packetBytes[6] << 8 | packetBytes[7]);
+                        double channel3 = (int)(packetBytes[8] << 8 | packetBytes[9]);
+                        /*
+                        Console.WriteLine(packetBytes[4] << 8 | packetBytes[5]);
+                        Console.WriteLine(packetBytes[6] << 8 | packetBytes[7]);
+                        Console.WriteLine(packetBytes[8] << 8 | packetBytes[9]);
+                        */
+                        basepointAverage += (channel1 + channel2 + channel3) / (double)3;
+                        //max = average > max ? average : max;
+                        index = 0;
+                        break;
+                    }
+                    packetBytes[index++] = serialPort.ReadByte();
+
+                }
+            }
+
+            basepointAverage /= 1000;
+            basepoint = basepointAverage;
+            //Console.WriteLine(basepoint);
+
+        }
+
+        //**DEPRECATED** -- Use getCalibrationArray() and then setGoal
+        //Starts Calibration for measuring maximum
+        /*
+        public void calibrateMax()
+        {
+            serialPort.DiscardInBuffer();
+            double[] runningArray = new double[26];
+            int currentIndex = 0;
+            for (int i = 0; i < 800; i++)
+            {
+                while (true)
+                {
+                    //Console.WriteLine(i);
+                    packetBytes[0] = serialPort.ReadByte();
+                    if (packetBytes[0] == 0xa5)
+                    {
+                        //Console.WriteLine("Found a5");
+                        packetBytes[1] = serialPort.ReadByte();
+                        if (packetBytes[1] == 0x5a)
+                        {
+                            //Console.WriteLine("Found 5a");
+                            break;
+                        }
+
+                    }
+                }
+                while (true)
+                {
+
+                    if (index > 16)
+                    {
+
+                        double[] channels = new double[6];
+                        double average = 0;
+                        for (int j = 0; j < channels.Length; j += 2)
+                        {
+                            channels[j] = transform((int)(packetBytes[j + 4] << 8 | packetBytes[j + 5]));
+                            average += channels[j];
+
+                        }
+
+
+                        runningArray[currentIndex++] = (double)average/channels.Length;
+                        if (currentIndex >= 26)
+                        {
+                            currentIndex = 0;
+                        }
+                        double runningAverageMax = 0;
+                        foreach(double k in runningArray)
+                        {
+                            runningAverageMax += k;
+                        }
+                        max = (runningAverageMax / runningArray.Length) > max ? (runningAverageMax / runningArray.Length) : max;
+                        //max = average > max ? average : max;
+                        index = 2;
+                        break;
+                    }
+
+                    packetBytes[index++] = serialPort.ReadByte();
+
+
+
+                }
+            }
+
+            Debug.Log(max);
+            //Console.WriteLine(max);
+
+        }*/
+
+        //Use instead of calibrateMax to get data for the next CALIBRATION_TIME seconds
+        public double[] getCalibrationArray()
+        {
+            serialPort.DiscardInBuffer();
+            double[] runningArray = new double[CALIBRATION_TIME*RATE];
+            int currentIndex = 0;
+            for (int i = 0; i < CALIBRATION_TIME * RATE; i++)
+            {
+                while (true)
+                {
+                    //Console.WriteLine(i);
+                    packetBytes[0] = serialPort.ReadByte();
+                    if (packetBytes[0] == 0xa5)
+                    {
+                        //Console.WriteLine("Found a5");
+                        packetBytes[1] = serialPort.ReadByte();
+                        if (packetBytes[1] == 0x5a)
+                        {
+                            //Console.WriteLine("Found 5a");
+                            break;
+                        }
+
+                    }
+                }
+                while (true)
+                {
+
+                    if (index > 16)
+                    {
+                        double[] channels = new double[6];
+                        double average = 0;
+                        for (int i = 0; i < channels.Length; i += 2)
+                        {
+                            channels[i] = transform((int)(packetBytes[i + 4] << 8 | packetBytes[i + 5]));
+                            average += channels[i];
+                        }
+
+                        /*
+                        Console.WriteLine(packetBytes[4] << 8 | packetBytes[5]);
+                        Console.WriteLine(packetBytes[6] << 8 | packetBytes[7]);
+                        Console.WriteLine(packetBytes[8] << 8 | packetBytes[9]);
+                        */
+                        average /= channels.Length;
+                        //max = average > max ? average : max;
+                        deletedValue = pastValues[pastIndex];
+
+                        pastValues[pastIndex++] = average;
+                        calculateRunningAverage(average);
+
+                        if (pastIndex >= pastValues.Length)
+                        {
+                            pastIndex = 0;
+                        }
+
+                        runningArray[currentIndex++] = this.runningAverage;
+
+                        index = 2;
+                        break;
+                    }
+                    packetBytes[index++] = serialPort.ReadByte();
+
+
+
+                }
+            }
+
+            return runningArray;
+
+        }
+
+
+
+        //Set the max value
+        /*
+        public void setMax(double max)
+        {
+            this.max = max;
+        }*/
+
+        public void setFlag(bool val){
+          this.keepRunning = val;
+        }
+
+        public void setGoal(double goal)
+        {
+            this.goal = goal;
+        }
+
+        public void close(){
+          serialPort.Close();
+        }
+
+        //Main to test code
+        public static void Main(string[] args)
+        {
+            EMGReader test = new EMGReader();
+
+            Console.ReadLine();
+            //test.calibrateBase();
+            Console.WriteLine("Done");
+            Console.ReadLine();
+           // test.calibrateMax();
+            Console.WriteLine("Done");
+            Console.ReadLine();
+            test.run();
+        }
+    }
+}
---
+++ HMSpace/Assets/Graph/Scripts/WindowGraph.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowGraph : MonoBehaviour
+{
+    [SerializeField] public Sprite circleSprite;
+    private GameObject graph;
+    private RectTransform graphContainer;
+    private RectTransform labelTemplateX;
+    private RectTransform labelTemplateY;
+    private RectTransform dashTemplateX;
+    private RectTransform dashMaxTemplate;
+    private RectTransform dashMaxInst = null;
+    private List<GameObject> gameObjectList;
+
+
+    private float yMaximum = 0;
+    private float yMinimum = 0;
+    [SerializeField] public Camera UICam;
+
+    private void Awake()
+    {
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+        labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>();
+        dashTemplateX = graphContainer.Find("dashTemplateX").GetComponent<RectTransform>();
+        dashMaxTemplate = graphContainer.Find("dashMax").GetComponent<RectTransform>();
+        gameObjectList = new List<GameObject>();
+
+        List<int> valueList = new List<int>() { 0, 100, 20, 99, 20, 56, 30, 22, 88, 77, 13, 95, 86, 60, 66, 22, 59, 75, 5, 20, 99, 20, 56, 30, 22, 88, 77, 13, 20, 99, 20, 56, 30, 22, 88, 77, 13, 95, 86, 60, 66, 22, 59, 75, 5, 20
+        , 100, 20, 99, 20, 56, 30, 22, 88, 77, 13, 95, 86, 60, 66, 22, 59, 75, 5, 20, 99, 20, 56, 30, 22, 88, 77, 13, 20, 99, 20, 56, 30, 22, 88, 77, 13, 95, 86, 60, 66, 22, 59, 75, 5, 20
+        , 100, 20, 99, 20, 56, 30, 22, 88, 77, 13, 95, 86, 60, 66, 22, 59, 75, 5, 20, 99, 20, 56, 30, 22, 88, 77, 13, 20, 99, 20, 56, 30, 22, 88, 77, 13, 95, 86, 60, 66, 22, 59, 75, 5, 20};
+
+        ShowGraph(valueList);
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(graphContainer, Input.mousePosition, UICam, out localPoint);
+            float yPos = (localPoint.y / graphContainer.sizeDelta.y) * (yMaximum - yMinimum) + yMinimum;
+            Debug.Log(yPos);
+            if(yPos < yMaximum && yPos > yMinimum)
+            {
+                if (dashMaxInst == null)
+                {
+                    dashMaxInst = Instantiate(dashMaxTemplate);
+                    dashMaxInst.SetParent(graphContainer, false);
+                    dashMaxInst.gameObject.SetActive(true);
+                }
+
+
+                dashMaxInst.anchoredPosition = new Vector2(0, (yPos - yMinimum) / (yMaximum - yMinimum) * graphContainer.sizeDelta.y);
+            }
+            
+        }
+        
+    }
+
+    private GameObject CreateCircle(Vector2 anchoredPosition)
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(5, 5);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        return gameObject;
+
+    }
+    
+    private void ShowGraph(List<int> valueList, int maxVisibleValueAmount = -1)
+    {
+
+        float graphWidth = graphContainer.sizeDelta.x;
+        float graphHeight = graphContainer.sizeDelta.y;
+        yMaximum = valueList[0];
+        yMinimum = valueList[0];
+
+        
+        if(maxVisibleValueAmount <= 0)
+        {
+            maxVisibleValueAmount = valueList.Count;
+        }
+
+        foreach(GameObject gameObject in gameObjectList)
+        {
+            Destroy(gameObject);
+
+        }
+        gameObjectList.Clear();
+
+        
+
+
+        for (int i = Mathf.Max(valueList.Count - maxVisibleValueAmount, 0); i < valueList.Count; i++)
+        {
+            int value = valueList[i];
+            if (value > yMaximum)
+            {
+                yMaximum = value;
+            }
+            if (value < yMinimum)
+            {
+                yMinimum = value;
+            }
+        }
+        float yDifference = yMaximum - yMinimum;
+        if(yDifference <= 0)
+        {
+            yDifference = 5f;
+        }
+        yMaximum = yMaximum + ((yDifference) * 0.1f);
+        yMinimum = yMinimum - ((yDifference) * 0.1f);
+
+
+
+        int xIndex = 0;
+        float xSize = graphWidth/(maxVisibleValueAmount + 1);
+        GameObject lastCircle = null;
+        for(int i = Mathf.Max(valueList.Count - maxVisibleValueAmount, 0); i < valueList.Count; i++)
+        {
+            float xPosition = xSize + xIndex * xSize;
+            float yPosition = ((valueList[i] - yMinimum) / (yMaximum - yMinimum)) * graphHeight;
+            GameObject circle = CreateCircle(new Vector2(xPosition, yPosition));
+            gameObjectList.Add(circle);
+            if(lastCircle != null)
+            {
+                GameObject gameObjectDotConnection = CreateDotConnection(lastCircle.GetComponent<RectTransform>().anchoredPosition, circle.GetComponent<RectTransform>().anchoredPosition);
+                gameObjectList.Add(gameObjectDotConnection);
+            }
+            lastCircle = circle;
+
+            /*RectTransform labelX = Instantiate(labelTemplateX);
+            labelX.SetParent(graphContainer);
+            labelX.gameObject.SetActive(true);
+            labelX.anchoredPosition = new Vector2(xPosition, -20f);
+            labelX.GetComponent<Text>().text = i.ToString();
+            labelX.localScale = new Vector2(1, 1);
+            labelX.transform.localPosition = new Vector3(labelX.transform.localPosition.x, labelX.transform.localPosition.y, 1);
+            gameObjectList.Add(labelX.gameObject);*/
+
+            xIndex++;
+        }
+        int separatorCount = 10;
+        for (int j = 0; j <= separatorCount; j++)
+        {
+            RectTransform labelY = Instantiate(labelTemplateY);
+            labelY.SetParent(graphContainer, false);
+            labelY.gameObject.SetActive(true);
+            float normalizedValue = j * 1f / separatorCount;
+            labelY.anchoredPosition = new Vector2(-20f, normalizedValue * graphHeight);
+            labelY.GetComponent<Text>().text = Mathf.RoundToInt(yMinimum + (normalizedValue * (yMaximum - yMinimum))).ToString();
+            labelY.localScale = new Vector2(1, 1);
+            gameObjectList.Add(labelY.gameObject);
+
+            RectTransform dashX = Instantiate(dashTemplateX);
+            dashX.SetParent(graphContainer, false);
+            dashX.gameObject.SetActive(true);
+            dashX.anchoredPosition = new Vector2(0, normalizedValue * graphHeight);
+            dashX.localScale = new Vector2(1, 1);
+            gameObjectList.Add(dashX.gameObject);
+        }
+    }
+
+    private GameObject CreateDotConnection(Vector2 dotPosA, Vector2 dotPosB)
+    {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPosB - dotPosA).normalized;
+        float distance = Vector2.Distance(dotPosA, dotPosB);
+        rectTransform.sizeDelta = new Vector2(distance, 2f);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.anchoredPosition = dotPosA + dir * distance * 0.5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+        return gameObject;
+    }
+}
