a6927ec32e586174c12aa8084438e18adb3c77af
Saferer <james.nguy@shaw.ca>
2020-09-14 01:40:27 -0600

    Player Movement and Coin Spawning

---
+++ HMSpace/Assets/GameFiles/Sections/RedCoin/Scripts/Coin/CoinSpawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SysDiag = System.Diagnostics;
+using System;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+
+    private Vector3 ScreenTopPosition;
+    private Vector3 ScreenBotPosition;
+
+    private float MinimumPositionOffset = 1.3f;
+    public GameObject Coin;
+
+    private bool StartSpawn = true;
+
+    private int NumberSpawned = 0;
+
+    private SysDiag.Stopwatch IntervalTimer;
+
+    private SysDiag.Stopwatch SpawnTimer;
+
+    private System.Random random;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        random = new System.Random();
+        IntervalTimer = new SysDiag.Stopwatch();
+        SpawnTimer = new SysDiag.Stopwatch();
+        ScreenTopPosition = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        ScreenBotPosition = Camera.main.ScreenToWorldPoint(Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StartSpawn)
+        {
+            StartSpawn = false;
+            SpawnTimer.Reset();
+            SpawnTimer.Start();
+            IntervalTimer.Reset();
+            IntervalTimer.Start();
+        }
+
+        if (NumberSpawned > 2)
+        {
+            NumberSpawned = 0;
+            IntervalTimer.Stop();
+            IntervalTimer.Reset();
+        }
+
+        if (IntervalTimer.ElapsedMilliseconds > 400)
+        {
+            IntervalTimer.Restart();
+            GameObject coin = Instantiate(Coin,
+            new Vector3(14, (ScreenTopPosition.y - ScreenBotPosition.y - MinimumPositionOffset) * 0.5f + ScreenBotPosition.y + MinimumPositionOffset, 0),
+            Coin.transform.rotation);
+            coin.SetActive(true);
+            NumberSpawned++;
+        }
+
+        if (SpawnTimer.ElapsedMilliseconds > random.Next(5000, 7000))
+        {
+            SpawnTimer.Stop();
+            SpawnTimer.Reset();
+            StartSpawn = true;
+        }
+    }
+}
---
+++ HMSpace/Assets/GameFiles/Sections/RedCoin/Scripts/Coin/CoinTransform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinTransform : MonoBehaviour
+{
+    public float Speed = -0.02f;
+
+    public float LastFrameTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LastFrameTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = (Time.time - LastFrameTime) * Speed;
+        LastFrameTime = Time.time;
+        transform.position = new Vector3(transform.position.x + distance, transform.position.y, transform.position.z);
+
+        if (transform.position.x < -15f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
